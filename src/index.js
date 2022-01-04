f98280e9688fd06e6fe4022a522087e146a0be2c
Vova <70287671+neshchadym@users.noreply.github.com>
2022-01-04 16:19:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-module.exports = function reverse (n) {
- let number = Math.abs(n);
+module.exports = function reverse(n) {
+    let number = Math.abs(n);
     let arr = number.toString().split("");
     let reversed = arr.reverse().join("");
     if (n > 0) {
         return -1 * reversed;
     }
-    return reversed; 
-}
+    if (n < 0) {
+        return reversed;
+    }
