431c327568c14cd973cd5f9126fb7c33eb636012
Vova <70287671+neshchadym@users.noreply.github.com>
2022-01-04 16:27:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse(n) {
-    (n > 0 ? 1 : -1) * Math.abs(n).toString().split('').reverse().join('')
+   return (n > 0 ? 1 : -1) * Math.abs(n).toString().split('').reverse().join('')
 }
